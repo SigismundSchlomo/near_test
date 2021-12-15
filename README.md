340905a16c0155a94bf3cc8b611fe229620087ac
Alex Kuznietsov <alex@kuznietsov.dev>
2021-12-15 16:31:23 +0200

    Add simple cross contract call

---
+++ README.md
@@ -0,0 +1,9 @@
+# The test project
+
+## Notes
+
+Before interaction with smart contract, user should register in the system and deposit some near.
+This near will be used to pay for storage that user's tokens will use. Could be retrieved back. 
+
+
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,76 @@
 //! This is a smart contract just to try some features of near_sdk
 
 
+use std::intrinsics::unreachable;
 use near_sdk::borsh::{self, BorshSerialize, BorshDeserialize};
-use near_sdk::{AccountId, env, near_bindgen, Promise, PromiseOrValue};
+use near_sdk::{
+    AccountId,
+    env,
+    ext_contract,
+    Gas,
+    near_bindgen,
+    Promise,
+    PromiseOrValue,
+    PromiseResult
+};
 use near_sdk::collections::{LookupMap, Vector};
+use near_sdk::json_types::ValidAccountId;
 
 near_sdk::setup_alloc!();
 
-const REF_EXCHANGE_ADDRESS: AccountId = if cfg!(feature = "main_net") {
-    "ref-finance.near".to_string()
-} else {
-    "ref-finance.testnet".to_string()
-};
+pub const GAS: Gas = 50_000_000_000_000;
 
+// pub const REF_EXCHANGE_ADDRESS: AccountId = if cfg!(feature = "main_net") {
+//     "v2.ref-finance.near".to_string()
+// } else {
+//     "ref-finance.testnet".to_string()
+// };
+
+pub const REF_EXCHANGE_ADDRESS: AccountId = "exchange.ref-dev.testnet".to_string();
 
 #[near_bindgen()]
 #[derive(Default, BorshSerialize, BorshDeserialize)]
-pub struct Contract {
-    user_accounts_values: LookupMap<AccountId, u128>,
+pub struct Contract;
+
+#[ext_contract(ext_ref_finance)]
+pub trait RefFinance {
+    fn storage_deposit(&mut self, account_id: Option<ValidAccountId>, registration_only: Option<bool>) -> StorageBalance;
+    fn get_whitelisted_tokens(&self) -> Vec<AccountId>;
 }
 
-impl Contract {
+#[ext_contract(ext_self)]
+pub trait ExtSelf {
+    fn handle_get_whitelisted_tokens_result() -> Vec<AccountId>;
+}
 
-    pub fn set_value(&mut self, &value: u128) {
-        let sender_id = env::predecessor_account_id();
-        self.user_accounts_values.insert(AccountId, value);
+#[near_bindgen()]
+impl Contract {
+    pub fn get_whitelisted_tokens(&self) -> Promise {
+        ext_ref_finance::get_whitelisted_tokens(&REF_EXCHANGE_ADDRESS, 0, GAS).then(
+            ext_self::handle_get_whitelisted_tokens_result(
+                &env::current_account_id(),
+                0,
+                GAS,
+            ),
+        )
     }
 
-
+    #[private]
+    pub fn callback_promise_result(&mut self) -> Vec<AccountId> {
+        assert_eq!(env::promise_results_count(), 1, "ERR_TOO_MANY_RESULTS");
+        match env::promise_result(0) {
+            PromiseResult::NotReady => unreachable!(),
+            PromiseResult::Successful(val) => {
+                if let Ok(whitelist) = near_sdk::serde_json::from_slice::<Vec<AccountId>>(&val) {
+                    whitelist
+                } else {
+                    env::panic(b"ERR_WRONG_VAL_RECEIVED")
+                }
+            },
+            PromiseResult::Failed => env::panic(b"ERR_CALL_FAILED"),
+        }
+    }
 }
+
+
+
