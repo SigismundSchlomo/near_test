db9a91713c87708879e4f5610181901aa96a7220
Alex Kuznietsov <alex@kuznietsov.dev>
2021-12-20 18:42:18 +0200

    Add another one method for experiment

--- near-test/src/lib.rs
+++ near-test/src/lib.rs
@@ -1,24 +1,20 @@
 //! This is a smart contract just to try some features of near_sdk
 
-
+use std::intrinsics::unreachable;
 use near_contract_standards::storage_management::StorageBalance;
-use near_sdk::borsh::{self, BorshSerialize, BorshDeserialize};
-use near_sdk::{
-    AccountId,
-    env,
-    ext_contract,
-    Gas,
-    near_bindgen,
-    Promise,
-    PromiseOrValue,
-    PromiseResult,
-};
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::collections::{LookupMap, Vector};
 use near_sdk::json_types::ValidAccountId;
+use near_sdk::{
+    env, ext_contract, near_bindgen, AccountId, Gas, Promise, PromiseOrValue, PromiseResult,
+};
+use crate::ref_utils::PoolInfo;
+
+mod ref_utils;
 
 near_sdk::setup_alloc!();
 
-pub const GAS: Gas = 50_000_000_000_000;
+pub const GAS: Gas = 300_000_000_000_000;
 
 // pub const REF_EXCHANGE_ADDRESS: &str = if cfg!(feature = "main_net") {
 //     "v2.ref-finance.near".to_string()
@@ -27,65 +23,70 @@ pub const GAS: Gas = 50_000_000_000_000;
 // };
 
 pub const REF_EXCHANGE_ADDRESS: &str = "exchange.ref-dev.testnet";
+//TODO: Move some code to other modules
+//TODO: Explore env::promise_results possibilities
+//TODO: Write some more pool related and swap related code
+//TODO: explore available tokens - get whitelisted is very useful for this one
+//TODO: Discuss about fat frontend strategy to simplify smart contract development and make whole project cheaper
+
+//TODO: Move ref finance related code to the separate module
+#[ext_contract(ext_ref_finance)]
+pub trait RefFinance {
+    fn storage_deposit(
+        &mut self,
+        account_id: Option<ValidAccountId>,
+        registration_only: Option<bool>,
+    ) -> StorageBalance;
+    fn get_whitelisted_tokens(&self) -> Vec<AccountId>;
+    fn get_number_of_pools(&self) -> u64;
+    fn get_pools(&self, from_index: u64, limit: u64) -> Vec<PoolInfo>;
+}
+
+// Если информация не будет нужна в смарт контракте - гораздо дешевле запрашивать информацию на фронтенд напрямую из контрактов ref finance
+#[ext_contract(ext_self)]
+pub trait ExtSelf {
+    fn get_whitelisted_tokens_callback() -> Vec<AccountId>;
+    fn get_number_of_pools_callback() -> u64;
+    fn get_pools_callback() -> Vec<PoolInfo>;
+}
 
 #[near_bindgen()]
 #[derive(Default, BorshSerialize, BorshDeserialize)]
-pub struct Contract {
-    message: String,
-}
-
-#[ext_contract(ext_ref_finance)]
-pub trait RefFinance {
-    fn storage_deposit(&mut self, account_id: Option<ValidAccountId>, registration_only: Option<bool>) -> StorageBalance;
-    fn get_whitelisted_tokens(&self) -> Vec<AccountId>;
-    fn get_number_of_pools(&self) -> u64;
-}
-
-#[ext_contract(ext_self)]
-pub trait ExtSelf {
-    fn handle_get_whitelisted_tokens_result() -> Vec<AccountId>;
-    fn handle_number_of_pools() -> u64;
-}
+pub struct Contract {}
 
 #[near_bindgen()]
 impl Contract {
-    pub fn set_message(&mut self, message: String) {
-        self.message = message;
-    }
-
-    pub fn get_message(&self) -> String {
-        self.message.clone()
-    }
-
     pub fn get_whitelisted_tokens(&self) -> Promise {
-        let prepaid_gas = env::prepaid_gas();
-        ext_ref_finance::get_whitelisted_tokens(&REF_EXCHANGE_ADDRESS.to_string(), 0, prepaid_gas).then(
-            ext_self::handle_get_whitelisted_tokens_result(
+        ext_ref_finance::get_whitelisted_tokens(&REF_EXCHANGE_ADDRESS.to_string(), 0, 20_000_000_000_000)
+            .then(ext_self::get_whitelisted_tokens_callback(
                 &env::current_account_id(),
                 0,
-                prepaid_gas
-            ),
-        )
+                20_000_000_000_000,
+            ))
     }
 
     pub fn get_number_of_pools(&self) -> Promise {
-        let prepaid_gas = env::prepaid_gas();
-        ext_ref_finance::get_number_of_pools(&REF_EXCHANGE_ADDRESS.to_string(), 0, prepaid_gas).then(
-            ext_self::handle_number_of_pools(
+        ext_ref_finance::get_number_of_pools(&REF_EXCHANGE_ADDRESS.to_string(), 0, 20_000_000_000_000)
+            .then(ext_self::get_number_of_pools_callback(
                 &env::current_account_id(),
                 0,
-                prepaid_gas
-            ),
-        )
+                20_000_000_000_000,
+            ))
     }
 
-    pub fn get_simple(&self) -> Promise {
-        let prepaid_gas = env::prepaid_gas();
-        ext_ref_finance::get_whitelisted_tokens(&REF_EXCHANGE_ADDRESS.to_string(), 0, prepaid_gas)
+    pub fn get_pools(&self, from_index: u64, limit: u64) -> Promise {
+        ext_ref_finance::get_pools(from_index, limit, &REF_EXCHANGE_ADDRESS.to_string(), 0, 20_000_000_000_000)
+            .then(ext_self::get_pools_callback(
+                &env::current_account_id(),
+                0,
+                20_000_000_000_000,
+            ))
     }
+
+    //TODO: Move callbacks in separate module
 
     #[private]
-    pub fn handle_get_whitelisted_tokens_result(&mut self) -> Vec<AccountId> {
+    pub fn get_whitelisted_tokens_callback(&mut self) -> Vec<AccountId> {
         assert_eq!(env::promise_results_count(), 1, "ERR_TOO_MANY_RESULTS");
         match env::promise_result(0) {
             PromiseResult::NotReady => unreachable!(),
@@ -100,7 +101,9 @@ impl Contract {
         }
     }
 
-    pub fn handle_number_of_pools(&mut self) -> u64 {
+    //TODO: Try to user callback attribute
+    #[private]
+    pub fn get_number_of_pools_callback(&mut self) -> u64 {
         match env::promise_result(0) {
             PromiseResult::NotReady => unreachable!(),
             PromiseResult::Successful(val) => {
@@ -109,58 +112,27 @@ impl Contract {
                 } else {
                     env::panic(b"ERR_WRONG_VAL_RECEIVED")
                 }
-            },
+            }
             PromiseResult::Failed => env::panic(b"ERR_CALL_FAILED"),
+        }
+    }
+
+    pub fn get_pools_callback(&mut self) -> Vec<PoolInfo> {
+        match env::promise_result(0) {
+            PromiseResult::NotReady => unreachable!(),
+            PromiseResult::Successful(val) => {
+                if let Ok(results) = near_sdk::serde_json::from_slice::<Vec<PoolInfo>>(&val) {
+                    results
+                } else {
+                    env::panic(b"ERR_WRONG_VAL_RECEIVED")
+                }
+            },
+            PromiseResult::Failed => env::panic(b"ERR_CALL_FAILED")
         }
     }
 }
 
-
 #[cfg(test)]
 mod test {
-    use super::*;
-    use near_sdk::MockedBlockchain;
-    use near_sdk::{testing_env, VMContext};
-    use near_sdk::serde::de::Unexpected::Str;
 
-    fn get_context(predecessor_account_id: String, storage_usage: u64) -> VMContext {
-        VMContext {
-            current_account_id: "some.cool.testnet".to_string(),
-            signer_account_id: "some.signer.testnet".to_string(),
-            signer_account_pk: vec![0, 1, 2, 3],
-            predecessor_account_id,
-            input: vec![],
-            block_index: 0,
-            block_timestamp: 0,
-            account_balance: 0,
-            account_locked_balance: 0,
-            storage_usage,
-            attached_deposit: 0,
-            prepaid_gas: 10u64.pow(18),
-            random_seed: vec![1, 2, 3],
-            is_view: false,
-            output_data_receivers: vec![],
-            epoch_height: 10,
-        }
-    }
-
-    #[test]
-    fn set_message() {
-        let context = get_context("hello.testnet".to_string(), 0);
-        testing_env!(context);
-        let mut contract = Contract { message: String::new() };
-        let message = "Hello".to_string();
-        contract.set_message(message.clone());
-        assert_eq!(contract.message, message, "Expected message to contain \"Hello\"");
-    }
-
-    #[test]
-    fn get_message() {
-        let context = get_context("hello.testnet".to_string(), 0);
-        testing_env!(context);
-        let mut contract = Contract { message: String::new() };
-        let message = "Hello".to_string();
-        contract.set_message(message.clone());
-        assert_eq!(contract.get_message(), message, "Expected to return \"Hello\"")
-    }
 }
---
+++ near-test/src/ref_utils.rs
@@ -0,0 +1,19 @@
+use near_sdk::AccountId;
+use near_sdk::json_types::U128;
+
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "near_sdk::serde")]
+#[cfg_attr(not(target_arch = "wasm32"), derive(Debug, PartialEq))]
+pub struct PoolInfo {
+    /// Pool kind.
+    pub pool_kind: String,
+    /// List of tokens in the pool.
+    pub token_account_ids: Vec<AccountId>,
+    /// How much NEAR this contract has.
+    pub amounts: Vec<U128>,
+    /// Fee charged for swap.
+    pub total_fee: u32,
+    /// Total number of shares.
+    pub shares_total_supply: U128,
+    pub amp: u64,
+}
