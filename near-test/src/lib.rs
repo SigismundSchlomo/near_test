1c6ac417dd4a93ba22b51d78a60b6bbd28d541d4
Alex Kuznietsov <alex@kuznietsov.dev>
2021-12-21 11:27:49 +0200

    swap integration researched

--- near-test/src/lib.rs
+++ near-test/src/lib.rs
@@ -1,29 +1,19 @@
-//! This is a smart contract just to try some features of near_sdk
-
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::json_types::ValidAccountId;
-use near_sdk::{
-    env, ext_contract, near_bindgen, AccountId, Gas, Promise, PromiseResult,
-};
-use crate::ref_utils::PoolInfo;
+use near_sdk::json_types::{ValidAccountId, U128};
+use near_sdk::{env, ext_contract, near_bindgen, AccountId, Gas, Promise, PromiseResult};
 
+use crate::ref_utils::{PoolInfo, SwapAction};
+
+mod callbacks;
 mod ref_utils;
 
 near_sdk::setup_alloc!();
 
 pub const GAS: Gas = 300_000_000_000_000;
-
-// pub const REF_EXCHANGE_ADDRESS: &str = if cfg!(feature = "main_net") {
-//     "v2.ref-finance.near".to_string()
-// } else {
-//     "ref-finance.testnet".to_string()
-// };
-
+pub const REFERRAL_ACCOUNT: &str = "kuznietsov.testnet";
 pub const REF_EXCHANGE_ADDRESS: &str = "exchange.ref-dev.testnet";
-//TODO: Move some code to other modules
+
 //TODO: Explore env::promise_results possibilities
-//TODO: Write some more pool related and swap related code
-//TODO: explore available tokens - get whitelisted is very useful for this one
 //TODO: Discuss about fat frontend strategy to simplify smart contract development and make whole project cheaper
 
 //TODO: Move ref finance related code to the separate module
@@ -37,15 +27,9 @@ pub trait RefFinance {
     fn get_whitelisted_tokens(&self) -> Vec<AccountId>;
     fn get_number_of_pools(&self) -> u64;
     fn get_pools(&self, from_index: u64, limit: u64) -> Vec<PoolInfo>;
+    fn swap(&mut self, actions: Vec<SwapAction>, referral_id: String) -> U128;
 }
 
-// Если информация не будет нужна в смарт контракте - гораздо дешевле запрашивать информацию на фронтенд напрямую из контрактов ref finance
-#[ext_contract(ext_self)]
-pub trait ExtSelf {
-    fn get_whitelisted_tokens_callback() -> Vec<AccountId>;
-    fn get_number_of_pools_callback() -> u64;
-    fn get_pools_callback() -> Vec<PoolInfo>;
-}
 
 #[near_bindgen()]
 #[derive(Default, BorshSerialize, BorshDeserialize)]
@@ -54,82 +38,86 @@ pub struct Contract {}
 #[near_bindgen()]
 impl Contract {
     pub fn get_whitelisted_tokens(&self) -> Promise {
-        ext_ref_finance::get_whitelisted_tokens(&REF_EXCHANGE_ADDRESS.to_string(), 0, 20_000_000_000_000)
-            .then(ext_self::get_whitelisted_tokens_callback(
-                &env::current_account_id(),
-                0,
-                20_000_000_000_000,
-            ))
+        ext_ref_finance::get_whitelisted_tokens(
+            &REF_EXCHANGE_ADDRESS.to_string(),
+            0,
+            20_000_000_000_000,
+        )
+        .then(ext_self::get_whitelisted_tokens_callback(
+            &env::current_account_id(),
+            0,
+            20_000_000_000_000,
+        ))
     }
 
     pub fn get_number_of_pools(&self) -> Promise {
-        ext_ref_finance::get_number_of_pools(&REF_EXCHANGE_ADDRESS.to_string(), 0, 20_000_000_000_000)
-            .then(ext_self::get_number_of_pools_callback(
-                &env::current_account_id(),
-                0,
-                20_000_000_000_000,
-            ))
+        ext_ref_finance::get_number_of_pools(
+            &REF_EXCHANGE_ADDRESS.to_string(),
+            0,
+            20_000_000_000_000,
+        )
+        .then(ext_self::get_number_of_pools_callback(
+            &env::current_account_id(),
+            0,
+            20_000_000_000_000,
+        ))
     }
 
     pub fn get_pools(&self, from_index: u64, limit: u64) -> Promise {
-        ext_ref_finance::get_pools(from_index, limit, &REF_EXCHANGE_ADDRESS.to_string(), 0, 20_000_000_000_000)
-            .then(ext_self::get_pools_callback(
-                &env::current_account_id(),
-                0,
-                20_000_000_000_000,
-            ))
+        ext_ref_finance::get_pools(
+            from_index,
+            limit,
+            &REF_EXCHANGE_ADDRESS.to_string(),
+            0,
+            20_000_000_000_000,
+        )
+        .then(ext_self::get_pools_callback(
+            &env::current_account_id(),
+            0,
+            20_000_000_000_000,
+        ))
     }
 
-    //TODO: Move callbacks in separate module
-
-    #[private]
-    pub fn get_whitelisted_tokens_callback(&mut self) -> Vec<AccountId> {
-        assert_eq!(env::promise_results_count(), 1, "ERR_TOO_MANY_RESULTS");
-        match env::promise_result(0) {
-            PromiseResult::NotReady => unreachable!(),
-            PromiseResult::Successful(val) => {
-                if let Ok(whitelist) = near_sdk::serde_json::from_slice::<Vec<AccountId>>(&val) {
-                    whitelist
-                } else {
-                    env::panic(b"ERR_WRONG_VAL_RECEIVED")
-                }
-            }
-            PromiseResult::Failed => env::panic(b"ERR_CALL_FAILED"),
-        }
+    #[payable]
+    pub fn swap(&mut self, actions: Vec<SwapAction>) -> Promise {
+        ext_ref_finance::swap(
+            actions,
+            REFERRAL_ACCOUNT.to_string(),
+            &REF_EXCHANGE_ADDRESS.to_string(),
+            env::attached_deposit(),
+            20_000_000_000_000,
+        ).then(ext_self::swap_callback(
+            &env::current_account_id(),
+            0,
+            20_000_000_000_000,
+        ))
     }
 
-    //TODO: Try to user callback attribute
-    #[private]
-    pub fn get_number_of_pools_callback(&mut self) -> u64 {
-        match env::promise_result(0) {
-            PromiseResult::NotReady => unreachable!(),
-            PromiseResult::Successful(val) => {
-                if let Ok(number) = near_sdk::serde_json::from_slice::<u64>(&val) {
-                    number
-                } else {
-                    env::panic(b"ERR_WRONG_VAL_RECEIVED")
-                }
-            }
-            PromiseResult::Failed => env::panic(b"ERR_CALL_FAILED"),
-        }
-    }
+    //TODO: Decide if I need it.
 
-    pub fn get_pools_callback(&mut self) -> Vec<PoolInfo> {
-        match env::promise_result(0) {
-            PromiseResult::NotReady => unreachable!(),
-            PromiseResult::Successful(val) => {
-                if let Ok(results) = near_sdk::serde_json::from_slice::<Vec<PoolInfo>>(&val) {
-                    results
-                } else {
-                    env::panic(b"ERR_WRONG_VAL_RECEIVED")
-                }
-            },
-            PromiseResult::Failed => env::panic(b"ERR_CALL_FAILED")
-        }
-    }
+    // pub fn storage_deposit(
+    //     &mut self,
+    //     account_id: Option<ValidAccountId>,
+    //     registration_only: Option<bool>,
+    // ) -> Promise {
+    //     let account_id = account_id
+    //         .map(|a| a.into())
+    //         .unwrap_or(env::predecessor_account_id());
+    //     let registration_only = registration_only.unwrap_or(false);
+    //     ext_ref_finance::storage_deposit(
+    //         account_id,
+    //         registration_only,
+    //         &REF_EXCHANGE_ADDRESS.to_string(),
+    //         env::attached_deposit(),
+    //         20_000_000_000_000,
+    //     )
+    //     .then(ext_self::storage_deposit_callback(
+    //         &env::current_account_id(),
+    //         0,
+    //         20_000_000_000_000,
+    //     ))
+    // }
 }
 
 #[cfg(test)]
-mod test {
-
-}
+mod test {}
--- near-test/src/ref_utils.rs
+++ near-test/src/ref_utils.rs
@@ -18,3 +18,21 @@ pub struct PoolInfo {
     pub shares_total_supply: U128,
     pub amp: u64,
 }
+
+/// Single swap action.
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct SwapAction {
+    /// Pool which should be used for swapping.
+    pub pool_id: u64,
+    /// Token to swap from.
+    pub token_in: AccountId,
+    /// Amount to exchange.
+    /// If amount_in is None, it will take amount_out from previous step.
+    /// Will fail if amount_in is None on the first step.
+    pub amount_in: Option<U128>,
+    /// Token to swap into.
+    pub token_out: AccountId,
+    /// Required minimum amount of token_out.
+    pub min_amount_out: U128,
+}
